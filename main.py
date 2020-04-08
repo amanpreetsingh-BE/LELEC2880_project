21a37aef1d9fd93b0bf7866738ef2a97c6ade79e
Amanpreet singh <amanpreet@outlook.be>
2020-04-08 21:32:03 +0200

    first update

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: group B
+"""
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+
+
+########################################
+# Implementation of ODFM transreceiver #
+########################################
+
+#Generation of 4QAM symbols : OK 
+#modulation of signal : TX
+#demodulation of received signal + channel AWGN  : RX
+#BER calculation
+
+
+
+#####################
+# Power allocation  #
+#####################
+
+#######################
+# Channel estimation  #
+#######################
+
+#############################
+# Optimal Viterbi decoding  #
+#############################
+
+
+
